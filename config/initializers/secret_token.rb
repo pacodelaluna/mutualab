e297da10b7526c34faec6555d8f0b6f019e8f94e
François Turbelin <pacodelaluna@gmail.com>
2013-05-22 10:32:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mutualab::Application.config.secret_token = '221ed20fdf403ca6508bc02ac93bf84f55ae9edf158a43f2ae439cf4493853f292cffb592c9d4189549e58d2e711a057dad2f4a76c99ab32d5392a84ff73f487'
